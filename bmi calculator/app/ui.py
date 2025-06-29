b56744f37a9aeaa3fcc637e12d467a824a8c2028
SHAILESH S <100753992+shaileshgoku@users.noreply.github.com>
2025-06-29 18:52:23 +0530

    Update ui.py

--- bmi calculator/app/ui.py
+++ bmi calculator/app/ui.py
@@ -4,8 +4,8 @@ from storage import save_to_csv
 
 def run():
     st.title("BMI CALCULATOR")
-    weight = st.number_input("Enter your wight (kg): ")
-    height = st.number_input("Enter your height (cm): ")
+    weight = st.number_input("Enter your wight (kg): ",min_value=1.0)
+    height = st.number_input("Enter your height (cm): ",min_value=1.0)
 
     if st.button("Calculate"):
         bmi = calculate_bmi(weight, height)
